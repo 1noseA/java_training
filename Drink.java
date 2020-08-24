fbbc40cdfd18c3d9ec4278167c147abf402fa785
1noseA <1noseA2L@gmail.com>
2020-08-24 15:28:40 +0900

    [問5-6]String型のswitch

--- Drink.java
+++ Drink.java
@@ -5,33 +5,18 @@ public class Drink {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     try {
       System.out.println("飲み物は何が好きですか？");
-      System.out.println("1 オレンジジュース(a)");
-      System.out.println("2 コーヒー(b)");
-      System.out.println("3 どちらでもない(c)");
-      System.out.println("1,2,3のどれかを選んでください(a,b,cでも可)。");
+      System.out.println("あ＝オレンジジュース");
+      System.out.println("い＝コーヒー");
+      System.out.println("う＝どちらでもない");
+      System.out.println("あ、い、うのどれかを選んでください。");
+      // JavaSE7からはString型もswitch文を使える
       String line = reader.readLine();
-      // 入力画面で空文字だったときの対策
-      char c ;
-      if (line.length() > 0) {
-        c = line.charAt(0);
-      } else {
-        c = 'c';
-      }
-      // 以下の書き方も可
-      // char c ;
-      // if (line.equals("")) {
-      //   c = 'c';
-      // } else {
-      //   c = line.charAt(0);
-      // }
 
-      switch (c) {
-        case '1':
-        case 'a':
+      switch (line) {
+        case "あ":
         System.out.println("オレンジジュースです。");
         break;
-        case '2':
-        case 'b':
+        case "い":
         System.out.println("コーヒーです。");
         break;
         default:
