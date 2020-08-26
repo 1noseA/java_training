59a560fdb6d18f2352e4f56256dc42604a62aa5b
1noseA <1noseA2L@gmail.com>
2020-08-26 15:58:40 +0900

    [9]拡張for

--- ShowArgs.java
+++ ShowArgs.java
@@ -1,8 +1,9 @@
 public class ShowArgs {
   public static void main(String[] args) {
     System.out.println("args.lengthの値は" + args.length);
-    for (int i = 0; i < args.length; i++) {
-      System.out.println("args[" + i + "]の値は" + args[i] + "です。");
+    // 拡張for
+    for (String s : args) {
+      System.out.println(s);
     }
   }
 }
